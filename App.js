9bdc0e639cab18d224ac139a2031784da2b3e6aa
SChowdary9030 <130335063+SChowdary9030@users.noreply.github.com>
2023-04-11 13:36:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+
+  return (
+     <ChatEngine 
+     
+        height="100vh"
+        projectID="
+        a156cd43-0235-4bd1-b2ab-337fb99ff206"
+        userName="5079"
+        userSecret="Accionlabs@123"
+
+     />
+  )
+}
+
+
+export default App; 
